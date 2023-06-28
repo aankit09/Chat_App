731758010df140b99aaac6ca75e7f4f9019f0e17
aankit09 <aankitsanvedi@gmail.com>
2023-06-28 16:17:47 +0530

    Added some new Files

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,25 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Flutter Project Example\\chat_app\\chat_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=D:\\Flutter Project Example\\chat_app\\chat_app"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Flutter Project Example\\chat_app\\chat_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Flutter Project Example\\chat_app\\chat_app"
+  "FLUTTER_TARGET=D:\\Flutter Project Example\\chat_app\\chat_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Flutter Project Example\\chat_app\\chat_app\\.dart_tool\\package_config.json"
+)
